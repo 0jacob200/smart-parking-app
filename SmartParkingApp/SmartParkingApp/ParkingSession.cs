5decd1b596f93c7779391ab6b15a734a45559cda
0jacob200 <yakov2000>
2019-10-13 12:18:07 +0300

    mycode 1.5
    все починено

--- SmartParkingApp/SmartParkingApp/ParkingSession.cs
+++ SmartParkingApp/SmartParkingApp/ParkingSession.cs
@@ -1,1 +1,32 @@
-﻿using System;namespace ParkingApp{    class ParkingSession    {        // Date and time of arriving at the parking        public DateTime EntryDt { get; set; }        // Date and time of payment for the parking        public DateTime? PaymentDt { get; set; }        // Date and time of exiting the parking        public DateTime? ExitDt { get; set; }        // Total cost of parking        public decimal? TotalPayment { get; set; }        // Plate number of the visitor's car        public string CarPlateNumber { get; set; }        // Issued printed ticket        public int TicketNumber { get; set; }        public ParkingSession(DateTime entryDT, DateTime? paymentDT, DateTime? exitDT,            decimal? totalPay, string carPlateNum, int ticketNum)        {            EntryDt = entryDT;            PaymentDt = paymentDT;            ExitDt = exitDT;            TotalPayment = totalPay;            CarPlateNumber = carPlateNum;            TicketNumber = ticketNum;        }    }}
+﻿using System;
+
+namespace ParkingApp
+{
+    class ParkingSession
+    {
+        // Date and time of arriving at the parking
+        public DateTime EntryDt { get; set; }
+        // Date and time of payment for the parking
+        public DateTime? PaymentDt { get; set; }
+        // Date and time of exiting the parking
+        public DateTime? ExitDt { get; set; }
+        // Total cost of parking
+        public decimal? TotalPayment { get; set; }
+        // Plate number of the visitor's car
+        public string CarPlateNumber { get; set; }
+        // Issued printed ticket
+        public int TicketNumber { get; set; }
+
+        public ParkingSession(DateTime entryDT, DateTime? paymentDT, DateTime? exitDT,
+            decimal? totalPay, string carPlateNum, int ticketNum)
+        {
+            EntryDt = entryDT;
+            PaymentDt = paymentDT;
+            ExitDt = exitDT;
+            TotalPayment = totalPay;
+            CarPlateNumber = carPlateNum;
+            TicketNumber = ticketNum;
+        }
+
+    }
+}
--- SmartParkingApp/SmartParkingApp/User.cs
+++ SmartParkingApp/SmartParkingApp/User.cs
@@ -5,6 +5,5 @@
         public string Name { get; set; }
         public string CarPlateNumber { get; set; }
         public string Phone { get; set; }
-        
     }
 }
